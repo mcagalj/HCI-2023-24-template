59129b2feb17dc39b010d3958e1c090191d798f1
kula124 <kula124@gmail.com>
2023-11-07 16:10:29 +0100

    Vjezba 4: Style navigation

--- semester-project/components/Navbar.tsx
+++ semester-project/components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FC } from "react";
 
 interface NavbarProps {
@@ -6,13 +10,27 @@ interface NavbarProps {
   pages: Record<string, `/${string}`>;
 }
 
+const baseClass =
+  "uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 rounded-sm text-brand-purple-900 hover:bg-brand-purple-200";
+
 const Navbar: FC<NavbarProps> = ({ pages }) => {
+  const pathName = usePathname();
+
   return (
     <nav className="flex items-center justify-center p-4">
       <ul className="flex gap-2">
         {Object.entries(pages).map(([name, path]) => (
           <li key={name}>
-            <Link href={path}>{name}</Link>
+            <Link href={path}>
+              <span
+                className={clsx(baseClass, {
+                  "bg-brand-purple-700 text-brand-purple-100 pointer-events-none":
+                    path === pathName,
+                })}
+              >
+                {name}
+              </span>
+            </Link>
           </li>
         ))}
       </ul>
--- semester-project/tailwind.config.ts
+++ semester-project/tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        roboto: ["var(--font-roboto)"],
+        "roboto-condensed": ["var(--font-roboto-condensed)"],
+        inter: ["var(--font-inter)"],
+        playfair: ["var(--font-playfair)"],
+      },
+      colors: {
+        "brand-purple": {
+          50: "#F2F4FE",
+          100: "#D8DBFD",
+          200: "#C5CAFC",
+          300: "#ABB2FA",
+          400: "#9AA3F9",
+          500: "#818CF8",
+          600: "#757FE2",
+          700: "#5C63B0",
+          800: "#474D88",
+          900: "#363B68",
+        },
+        "brand-orange": {
+          50: "#FFF5E6",
+          600: "#E88C00",
+          800: "#8C5500",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
